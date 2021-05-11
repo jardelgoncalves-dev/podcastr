9be561ee7118b266301fbe6ac66537e0da926328
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-10 23:22:21 -0300

    feat: add global components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/global.scss'
+
+import { Header } from '../components/Header';
+
+import style from '../styles/app.module.scss';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={style.appContainer}>
+      <main>
+        <Header />
+        <Component {...pageProps} />;
+      </main>
+
+    </div>
+  )
 }
 
 export default MyApp;
