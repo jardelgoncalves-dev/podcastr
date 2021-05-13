44db8d4f0cb1bd6fbcb7ffcd8818711d16d595a7
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-12 23:44:47 -0300

    feat: get episodes from api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+import { GetStaticProps } from 'next'
+import { Header } from "../components/Header";
+
+export default function Home(props) {
+  console.log(props.episodes)
+  return <h1>Index</h1>;
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes');
+  const episodes = await response.json()
+  return {
+    props: {
+      episodes
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
