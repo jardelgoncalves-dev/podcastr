2686a1b9b1e355b1d254bb14aa2140c71aa9701b
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-13 23:51:02 -0300

    refactor: change fetch by axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next'
 import { Header } from "../components/Header";
+import api from '../services/api';
 
 interface Episode {
   id: string
@@ -17,8 +18,7 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes');
-  const episodes = await response.json()
+  const { data: episodes } = await api.get('/episodes?_limit=2&_sort=published_at&_order=desc')
   return {
     props: {
       episodes
