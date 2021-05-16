d0d6c206321654bd5a7ca4b78235b84cbdcbe7f9
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-15 23:57:49 -0300

    feat: define inteface Episode

---
+++ src/models/Episode.ts
@@ -0,0 +1,11 @@
+export interface Episode {
+  id: string
+  title: string
+  description: string;
+  members: string;
+  thumbnail: string;
+  publisedAt: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+}
